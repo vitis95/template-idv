6aa38e37720d9b9ff234d7269e07f87739fc7fa3
vitis95 <37616023+vitis95@users.noreply.github.com>
2022-04-15 15:11:13 +0200

    collegamento foglio di stile in functions

--- functions.php
+++ functions.php
@@ -4,3 +4,7 @@
 add_theme_support('menus');
 add_theme_support('title-tag');
 
+function load_theme_scripts() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'load_theme_scripts');
