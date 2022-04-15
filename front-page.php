6d8e1866cb1cef38488fccc6de37cabd269e5613
vitis95 <37616023+vitis95@users.noreply.github.com>
2022-04-15 23:29:26 +0200

    the_content nella homepage (front page)

--- front-page.php
+++ front-page.php
@@ -1,10 +1,20 @@
 <?php get_header(); ?>
-<h1>Home</h1>
 
+
+<h1><?php the_title() ?></h1>
 
 <h2><?php bloginfo('name'); ?></h2>
 <h3><?php bloginfo('description'); ?></h3>
 
+<div class="container">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php the_content(); ?>
+
+<?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+</div>
 
 
 
