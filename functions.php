53527981c0a4b3886467ed3a58b233b090edf967
vitis95 <37616023+vitis95@users.noreply.github.com>
2022-04-19 15:18:16 +0200

    function widget

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
 
 add_theme_support('menus');
 add_theme_support('title-tag');
+add_theme_support('widgets');
+add_theme_support('widgets-block-editor');
+
 
 function load_theme_scripts() {
     wp_enqueue_style('style', get_stylesheet_uri());
