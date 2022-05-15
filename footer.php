8c0ab3110806c9f83d3002e92bbf894b38a4b7d8
vitis95 <37616023+vitis95@users.noreply.github.com>
2022-05-15 23:28:25 +0200

    struttura footer

--- footer.php
+++ footer.php
@@ -1,8 +1,49 @@
 <div class="clearfix"></div>
-<footer><p>&copy; Copyright <?php echo date("o");?> - <?php bloginfo('name'); ?></p></footer>
+<!-- <footer><p>&copy; Copyright <?php echo date("o");?> - <?php bloginfo('name'); ?></p></footer> -->
+
+    <!-- Footer START -->
+    <footer class="site-footer">
+        <div class="footer">
+            <div class="footer-section">
+            <div class="footer-copy">
+                    <h3>Copyright</h3>
+                    <p>&copy; Copyright <?php echo date("o");?> - <?php bloginfo('name'); ?></p>
+                </div>
+                <div class="footer-newsletter">
+                    <h3>Iscriviti alla newsletter</h3>
+                    <p>Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy
+                        policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy </p>
+                    <div class="footer-newsletter-email">
+                        <input class="input-email" type="email">
+                        <input class="input-submit" type="submit">
+                    </div>
+                </div>
+                <div class="footer-social">
+                    <h3>Follow US</h3>
+                    <p>Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy
+                        policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy rivacy policy
+                        Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy
+                        policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy rivacy policy
+                        Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy
+                        policy Privacy policy Privacy policy Privacy policy Privacy policy Privacy policy </p>
+                </div>
+            </div>
+            <div class="footer-section">
+                <div class="footer-contact-us-wrapper">
+                    <p>Privacy policy Privacy policy Privacy policy</p><br>
+                    <p>Privacy policy Privacy policy Privacy policy</p><br>
+                    <p>Privacy policy Privacy policy Privacy policy</p><br>
+                    <p>Privacy policy Privacy policy Privacy policy</p><br>
+                    <p>Privacy policy Privacy policy Privacy policy</p><br>
+                </div>
+
+
+
+            </div>
+        </div>
+    </footer>
+    <!-- Footer END -->
 </div><!-- questo è la chiusura del container -->
-
-
 
 <?php wp_footer(); ?>
 </body>
